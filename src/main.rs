6cc8805a8a7046c21934429d798913c61658c226
Axel Sorenson <AxelPSorenson@gmail.com>
2024-09-28 23:05:12 -0700

    Application no longer starts on a hardcoded address

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use newsletter_website::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port"); // Port 0 triggers an OS scan for a random
+                                                                                          // available port which will then be bound to
+                                                                                          // this TcpListener
+    run(listener)?.await
 }
