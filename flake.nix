c25b9bac13c1f73b5406fcf7b7ef64d491457167
Axel Sorenson <AxelPSorenson@gmail.com>
2024-09-30 16:50:03 -0700

    PostgreSQL Docker script

--- flake.nix
+++ flake.nix
@@ -6,17 +6,43 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { 
-    self, 
-    nixpkgs, 
-    flake-utils, 
-    ... 
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
           inherit system;
         };
+        startDockerPostgreSQL = pkgs.writeShellScriptBin "start_docker_postgresql" ''
+          set -x
+          set -eo pipefail
+
+          # Check if a custom user has been set, otherwise default to 'postgres'
+          DB_USER=''${POSTGRES_USER:=postgres}
+
+          # Check if a custom password has been set, otherwise default to 'password'
+          DB_PASSWORD="''${POSTGRES_PASSWORD:=password}"
+
+          # Check if a custom database name has been set, otherwise default to 'newsletter'
+          DB_NAME="''${POSTGRES_DB:=newsletter}"
+
+          # Check if a custom port has been set, otherwise default to '5432'
+          DB_PORT="''${POSTGRES_PORT:=5432}"
+
+          # Launch postgres using Docker
+          docker run \
+              -e POSTGRES_USER=''${DB_USER} \
+              -e POSTGRES_PASSWORD=''${DB_PASSWORD} \
+              -e POSTGRES_DB=''${DB_NAME} \
+              -p "''${DB_PORT}":5432 \
+              -d postgres \
+              postgres -N 1000
+              # ^ Increased the maximum number of connections for testing purposes
+        '';
       in
         with pkgs; {
           # packages = {inherit} # inherit any defined derivations here
@@ -28,9 +54,10 @@
               rustfmt
               clippy
               taplo # TOML formatter and lsp
-              evcxr # Rust REPL 
+              evcxr # Rust REPL
               bacon
               grcov
+              alejandra # Nix Formatter
 
               cargo-watch
               cargo-nextest
@@ -65,7 +92,10 @@
             buildInputs = with pkgs; [
               pkg-config # Needed for openssl Nix package
               openssl # Needed for reqwest crate
-            ]; 
+
+              # Scripts
+              startDockerPostgreSQL
+            ];
             shellHook = ''
               export HELIX_RUNTIME="$PWD/runtime"
               export OPENSSL_NO_VENDOR=1
